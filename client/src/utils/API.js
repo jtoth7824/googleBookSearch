66def895ccc3b5ec90538d37d001eadc7db133f3
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-04-07 17:25:51 -0400

    initial commit

---
+++ client/src/utils/API.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const apiKey = "AIzaSyBE8rew2L7hzqHNcbX1zl03rA4mdytQuu8"
+
+export default {
+  //google api
+  getGoogleBooks: function() {
+      return axios.get("https://www.googleapis.com/books/v1/volumes?q=search-terms&key=" + apiKey);
+  },
+
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
---
+++ models/book.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
+});
+
+
+
+
+const Book = mongoose.model("Book", bookSchema);
+//const Book1 = mongoose.model("Book1", googleBookSchema);
+
+module.exports = Book;
+//module.exports = Book1;
