dfe03d7bc026a8bd12ffd4befa66206e72b2dd43
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-04-12 15:14:26 -0400

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // app.use(require('./routes/api/john.js'));
 
 console.log(process.env.REACT_APP_API_KEY)
